38896c778d088c2b2a423636890b11a486b2fdc8
Mury05 <murielleodilone@gmail.com>
2025-01-30 09:59:52 +0100

    Initialize Dart command-line application with basic structure and configuration

---
+++ bin/dart_todolist_console.dart
@@ -0,0 +1,5 @@
+import 'package:dart_todolist_console/dart_todolist_console.dart' as dart_todolist_console;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_todolist_console.calculate()}!');
+}
---
+++ test/dart_todolist_console_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_todolist_console/dart_todolist_console.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
